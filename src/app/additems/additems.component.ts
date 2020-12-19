f95d8af9596e42d9a5bb5398c678917ee9da1ee4
PedroVidal02 <pedrovidal2703@gmail.com>
2020-12-18 23:47:58 -0300

    changed namebox from input to textarea

--- src/app/additems/additems.component.ts
+++ src/app/additems/additems.component.ts
@@ -15,12 +15,12 @@ export class AdditemsComponent implements OnInit {
   @Output() studenttopass = new EventEmitter<Student>();
 
   passStudent(namestudent: string, jediCheck: boolean){
-    this.newstudent = {name: namestudent, isJedi: jediCheck, temple: 'Empty'}
+    this.newstudent = {name: namestudent, isJedi: jediCheck, temple: 'None'}
     
     this.studenttopass.emit(this.newstudent)
   }
 
-  clearinput(input1: HTMLInputElement) {
+  clearinput(input1: HTMLTextAreaElement) {
     input1.value = ''
     this.jediCheck = false
   }
